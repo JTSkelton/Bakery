f316d537d2a00376ac1955cec1b587c1f6a57e44
Jack <skelton.jt9@gmail.com>
2022-02-13 11:30:36 -0500

    added consol UI + bug for if 0 was entered

--- Bakery.Solution/Bakery/Models/Program.cs
+++ Bakery.Solution/Bakery/Models/Program.cs
@@ -7,13 +7,23 @@ public class Program
   {
     public static void Main()
     {
-
+      System.Console.WriteLine("Hello Welcome To The Bakery!!");
+      System.Console.WriteLine("Bread is $5 a loaf. Buy 2 get one freeeee!");
+      System.Console.WriteLine("Pasteries are $2 a piece. One dollar off orders over 1!");
+      System.Console.WriteLine("How much bread would you like?");
+      var bread = System.Console.ReadLine();
+      System.Console.WriteLine("How many pasteries would you like?");
+      var pasteries = System.Console.ReadLine();
+      Bread BreadCost = new Bread();
+      Pastery PasteryCost = new Pastery();
+      System.Console.WriteLine("Your total cost is: " + (BreadCost.MakeBread(Convert.ToInt32(bread)) + PasteryCost.MakePastery(Convert.ToInt32(pasteries))));
     }
   }
   public class Bread
   {
   public int MakeBread(int loaf)
     {
+      if (loaf == 0) return 0;
       if (loaf % 3 == 0) return ((loaf * 5)-((loaf * 5) / 3));
       
       return loaf * 5;
@@ -24,6 +34,7 @@ public class Program
   {
   public int MakePastery(int sweet)
     {
+      if (sweet == 0) return 0;
       if (sweet == 1) return sweet * 2;
 
       return ((sweet * 2) - 1);
