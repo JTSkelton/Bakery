8080115fbbd3c17066f4ff60f04f545f57028d09
Jack <skelton.jt9@gmail.com>
2022-02-13 10:47:08 -0500

    Added second test

--- Bakery.Solution/Bakery.Tests/ModelTests/ProgramTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/ProgramTests.cs
@@ -7,10 +7,17 @@ namespace Bakery.Tests
   public class ProgramTests
   {
     [TestMethod]
-    public void Bread_ReturnFalse_False()
+    public void Bread_ReturnLoafCoast_5()
     {
       Program testBread = new Program();
       Assert.AreEqual(5, testBread.Bread(1));
+    } 
+
+    [TestMethod]
+    public void Bread_Return3LoafCost_10()
+    {
+      Program testBread = new Program();
+      Assert.AreEqual(10, testBread.Bread(3));
     } 
   }
 }
